629ed2317e568e9a1da81f86b8d36837b796f73a
unknown <bloodmoney852@hotmail.com>
2022-08-08 15:36:05 +0300

    Base package structure is created

---
+++ src/main/java/com/todeb/batuhanayyildiz/creditapplicationsystem/CreditApplicationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.todeb.batuhanayyildiz.creditapplicationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditApplicationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditApplicationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todeb/batuhanayyildiz/creditapplicationsystem/CreditApplicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todeb.batuhanayyildiz.creditapplicationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

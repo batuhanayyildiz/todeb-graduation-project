530156882dbb8b226cc72eb878a382a05e44982b
Batuhan Ayyildiz <batuhanayyildiz06@gmail.com>
2022-08-10 02:25:12 +0300

    feat: creditApplicationDTO is created

---
+++ src/main/java/com/todeb/batuhanayyildiz/creditapplicationsystem/model/dto/CreditApplicationDTO.java
@@ -0,0 +1,22 @@
+package com.todeb.batuhanayyildiz.creditapplicationsystem.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.todeb.batuhanayyildiz.creditapplicationsystem.model.entity.CreditApplicationStatus;
+import com.todeb.batuhanayyildiz.creditapplicationsystem.model.entity.CreditLimit;
+import com.todeb.batuhanayyildiz.creditapplicationsystem.model.entity.Customer;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+public class CreditApplicationDTO {
+    @CreationTimestamp
+    @JsonFormat( pattern = "dd-MM-yyyy" )
+    private LocalDate creditApplicationDate;
+
+    @Enumerated(EnumType.STRING)
+    private CreditApplicationStatus applicationStatus;
+
+    private CreditLimit creditLimit;
+    private Customer customer;
+}
